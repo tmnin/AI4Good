64358c42c1f315ac8992c7154d028de555b5fb32
Tanay Kashyap <tanayk03@gmail.com>
2026-03-14 11:54:57 -0400

    L fix

--- backend/server.py
+++ backend/server.py
@@ -63,7 +63,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
